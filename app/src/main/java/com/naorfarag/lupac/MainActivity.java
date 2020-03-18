ec37988c62aad5a83eb8b162ba025733bddb2416
Sarel <sarelmicha93@gmail.com>
2020-03-18 23:14:53 +0200

    background video has been added

--- app/src/main/java/com/naorfarag/lupac/MainActivity.java
+++ app/src/main/java/com/naorfarag/lupac/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +17,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton newAlarmBtn;
+    private VideoView videoview;
     private Context context;
 
     @Override
@@ -23,12 +27,22 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         setIds();
         setClickListeners();
+        setBackground();
 
 
+    }
 
+    private void setBackground() {
 
-
-
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.starts);
+        videoview.setVideoURI(uri);
+        videoview.start();
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 
     private void setClickListeners() {
@@ -45,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setIds() {
+    private void setIds()
+    {
+        videoview = (VideoView) findViewById(R.id.background_video);
         newAlarmBtn = (FloatingActionButton) findViewById(R.id.new_alarm);
     }
 }
