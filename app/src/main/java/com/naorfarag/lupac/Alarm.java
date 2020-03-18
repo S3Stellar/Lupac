1780e64754141812e7a7000cdba816757a0b9c11
Sarel <sarelmicha93@gmail.com>
2020-03-18 15:55:21 +0200

    newAlarmActivity has been set up for the first time

--- app/src/main/java/com/naorfarag/lupac/Alarm.java
+++ app/src/main/java/com/naorfarag/lupac/Alarm.java
@@ -1,6 +1,7 @@
 package com.naorfarag.lupac;
 
 public class Alarm {
+    
     private int hour;
     private int minutes;
     private int seconds;
--- app/src/main/java/com/naorfarag/lupac/NewAlarmActivity.java
+++ app/src/main/java/com/naorfarag/lupac/NewAlarmActivity.java
@@ -1,4 +1,14 @@
 package com.naorfarag.lupac;
 
-public class NewAlarmActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_alarm);
+    }
 }
